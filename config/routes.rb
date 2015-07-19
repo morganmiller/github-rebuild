9a2184c67603f5a54fee663ebf98bc5c8dd30a9c
Morgan Miller <m.miller722@gmail.com>
2015-07-19 11:07:07 -0600

    add full login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   
   get '/auth/github', as: 'login'
   get '/auth/github/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
 end
